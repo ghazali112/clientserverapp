9d78c5057bfdf4f286dd7528e0eb5857e43fb45b
asad khan <auzair535@gmail.com>
2018-05-02 20:19:13 +0500

    my client server application

---
+++ oslab/oslab/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Drawing.Imaging;
+using System.Runtime.Serialization.Formatters.Binary;
+namespace oslab
+{
+    public partial class Form1 : Form
+    {
+        private readonly TcpClient client =new TcpClient();
+        private NetworkStream mainstream;
+        private int portnum;
+      
+        private static Image GrabDesktop() {
+            Rectangle bounds = Screen.PrimaryScreen.Bounds;
+            Bitmap screenshot = new Bitmap(bounds.Width,bounds.Height,PixelFormat.Format32bppArgb);
+            Graphics graphic =Graphics.FromImage(screenshot);
+            graphic.CopyFromScreen(bounds.X, bounds.Y, 0, 0, bounds.Size, CopyPixelOperation.SourceCopy);
+            return screenshot;
+        
+        
+        }
+
+        private void SendImage() {
+            BinaryFormatter binformatter = new BinaryFormatter();
+            mainstream = client.GetStream();
+            binformatter.Serialize(mainstream,GrabDesktop());
+
+        
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            portnum = int.Parse(textBox2.Text);
+            try
+            {
+                client.Connect(textBox1.Text,portnum);
+                MessageBox.Show("connected");
+
+
+            }
+            catch (Exception es)
+            {
+
+                MessageBox.Show(es.Message.ToString());
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button2.Text.StartsWith("connect")|| button2.Text.StartsWith("share")) 
+            {
+                timer1.Start();
+                button2.Text = "stop sharing";
+
+            
+            }
+            else {
+            
+            timer1.Stop();
+            button2.Text="share my screen";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            SendImage();
+        }
+    }
+}
---
+++ oslab2/oslab2/Form2.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+namespace oslab2
+{
+    public partial class Form2 : Form
+    {
+        public int port;
+        private TcpClient client;
+        private TcpListener Server;
+        private NetworkStream mainStream;
+        private readonly Thread Listening;
+        private readonly Thread Getimage;
+
+
+        public Form2( int Port )
+        {
+            port = Port;
+            client = new TcpClient();
+            Listening = new Thread(StartListening);
+            Getimage = new Thread(recieveimage);
+
+
+            InitializeComponent();
+        }
+
+        private void StartListening()
+        {
+           // while (true) {
+                
+                client = Server.AcceptTcpClient();
+
+               // MessageBox.Show("Start");
+                Getimage.Start();
+            //}
+            
+        
+        
+        }
+
+        private void StopListening() {
+
+            Server.Stop();
+            client = null;
+            if (Listening.IsAlive) Listening.Abort();
+            if (Getimage.IsAlive) Getimage.Abort();
+        
+        }
+
+        private void recieveimage() {
+
+            BinaryFormatter binformatter = new BinaryFormatter();
+            while (client.Connected) {
+                mainStream = client.GetStream();
+                pictureBox1.Image = (Image) binformatter.Deserialize(mainStream);
+            
+            }
+        }
+
+        protected override void OnLoad(EventArgs e ) {
+            base.OnLoad(e);
+            Server = new TcpListener(IPAddress.Any,port);
+            Server.Start();           
+		   Listening.Start();
+        
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e ){
+        base.OnFormClosing(e);
+        StopListening();
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
